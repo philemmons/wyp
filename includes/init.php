38ddd9608c91c268efb930c87c9e619c53352503
philemmons <phil_emmons@sbcglobal.net>
2026-04-25 19:51:04 -0700

    remove php info display; revisit comments in init.php and legacy some lines;

--- includes/init.php
+++ includes/init.php
@@ -1,12 +1,12 @@
 <?php
-// security settings
+// security
 ini_set('session.cookie_secure', 1);
 ini_set('session.cookie_httponly', 1);
 ini_set('session.cookie_samesite', 'Strict');
 ini_set('session.use_strict_mode', 1);
 ini_set('session.use_only_cookies', 1);
 
-// production errors -- 
+// production errors - controlled in cPanel
 // ini_set('display_errors', 0);
 // ini_set('log_errors', 1);
 // ini_set('error_log', __DIR__ . '/../logs/php-error.log');
