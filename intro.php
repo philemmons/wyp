8e8dbb7aa840a87eba49cf4cf9ce17d0de96efa7
philemmons <phil_emmons@sbcglobal.net>
2026-05-01 23:20:25 -0700

    resize icons

--- intro.php
+++ intro.php
@@ -78,7 +78,7 @@ require_once 'includes/header.php';
           </ul>
 
           <div class="dog-avatar-frame mt-4">
-            <img src='/images/chandra icon 55x55.png.png' alt="" width=35 height=35 aria-hidden="true">
+            <img src='/images/chandra icon 55x55.png' alt="chandra bust icon" width=55 height=55 aria-hidden="true">
           </div>
 
           <div class="dog-breed-badge">Chihuahua</div>
@@ -119,7 +119,7 @@ require_once 'includes/header.php';
           </ul>
 
           <div class="dog-avatar-frame mt-4">
-            <img src='/images/skipper-icon-50x42.png' alt="" width=35 height=30 aria-hidden="true">
+            <img src='/images/skipper-icon-50x42.png' alt="skipper cartoon icon" width=50 height=42 aria-hidden="true">
           </div>
 
           <div class="dog-breed-badge">Chihuahua and Jack Russell</div>
