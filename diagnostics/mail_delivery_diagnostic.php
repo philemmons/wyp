0e84f7f0090af21bd2b1c36f2a449ea423f630f3
philemmons <phil_emmons@sbcglobal.net>
2026-05-02 22:12:36 -0700

    test env vars

--- diagnostics/mail_delivery_diagnostic.php
+++ diagnostics/mail_delivery_diagnostic.php
@@ -1,4 +1,8 @@
 <?php
+header('Content-Type: text/plain');
+var_dump(getenv('WYP_DIAG_KEY'));
+var_dump($_SERVER['WYP_DIAG_KEY'] ?? null);
+var_dump($_ENV['WYP_DIAG_KEY'] ?? null);
 
 declare(strict_types=1);
 
