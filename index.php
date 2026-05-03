a5c0a953be518ff20e9c8ac0eac6541c24698840
philemmons <phil_emmons@sbcglobal.net>
2026-05-02 18:29:24 -0700

    layout token

--- index.php
+++ index.php
@@ -42,7 +42,7 @@ require_once 'includes/header.php';
       </div>
 
       <div class="col-lg-5 text-center d-none d-lg-block">
-        <img src="images/chandra-headshot.png" alt="chandra graphic logo" width="330" height="332">
+        <img src="images/chandra-headshot.png" alt="chandra had shot token" width="330" height="332" class="mb-3">
         <div class="hero-card hero-tagline">Chandra &amp; Skipper say hello!</div>
       </div>
 
