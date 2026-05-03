4e664f376bdb830a881903e6ee45c9d39c7c0881
philemmons <phil_emmons@sbcglobal.net>
2026-05-02 22:17:18 -0700

    echo test file is read

--- diagnostics/test.php
+++ diagnostics/test.php
@@ -1,6 +1,7 @@
 <?php
 
 header('Content-Type: text/plain');
+echo "yes, this is a test file";
 var_dump(getenv('WYP_DIAG_KEY'));
 var_dump($_SERVER['WYP_DIAG_KEY'] ?? null);
 var_dump($_ENV['WYP_DIAG_KEY'] ?? null);
